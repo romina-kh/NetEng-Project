eee103b09cac16b326c45ee5596a3bc1b3d9c8dd
unknown <romina.khanmohammadi99@gmail.com>
2025-12-16 20:20:36 +0330

    fix: form collaborate

--- frontend/net-eng/src/components/homePage/Collaborate.jsx
+++ frontend/net-eng/src/components/homePage/Collaborate.jsx
@@ -9,19 +9,19 @@ function Collaborate(){
             <h3>هم اکنون آغاز کنید</h3>
             <div className="colab-container">
                 <div className="inner-container">
-                    <div className="form">
+                    <div className="form-collaborate">
                         <input placeholder="نام خانوادگی" type="text" />
                         <input placeholder="*نام" type="text" />
                     </div>
-                    <div className="form">
+                    <div className="form-collaborate">
                         <input placeholder="آدرس ایمیل" type="text" />
                         <input placeholder="*شماره موبایل" type="text" />
                     </div>
-                    <div className="form">
+                    <div className="form-collaborate">
                         <input placeholder="نوع درخواست" type="text" />
                         <input placeholder="نام شرکت" type="text" />
                     </div>
-                    <div className="form">
+                    <div className="form-collaborate">
                         <input id="text-box" placeholder="متن درخواست" type="text" />
                     </div>
                 </div>
