6b121989ac71fea903f187e5f8e9f6a367e7d99b
unknown <romina.khanmohammadi99@gmail.com>
2025-12-16 23:51:21 +0330

    refactor: fixing slider arrow + style

--- frontend/net-eng/src/components/homePage/slider.jsx
+++ frontend/net-eng/src/components/homePage/slider.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import Slider from "react-slick";
 
 import "slick-carousel/slick/slick.css";
@@ -7,29 +7,38 @@ import "slick-carousel/slick/slick-theme.css";
 import slide1 from "../../assets/slide1.png";
 import slide2 from "../../assets/slide2.png";
 import slide3 from "../../assets/slide3.png";
-import "../../styles/homePage/slider.css"
+import "../../styles/homePage/slider.css";
 
 export default function MySlider() {
+  const sliderRef = useRef(null);
+
   const settings = {
     dots: true,
+    arrows: false, 
     infinite: true,
     speed: 600,
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
-    arrows: true,
     pauseOnHover: true,
     draggable: true,
-    swipeToSlide: true,
   };
 
   return (
-    <div className="slider-container">
-      <Slider {...settings}>
-        <div><img src={slide1} className="slider-img" /></div>
-        <div><img src={slide2} className="slider-img" /></div>
-        <div><img src={slide3} className="slider-img" /></div>
+    <div className="slider-wrapper">
+      <button className="slider-arrow left" onClick={() => sliderRef.current.slickPrev()}>›</button>
+      <button className="slider-arrow right" onClick={() => sliderRef.current.slickNext()}>‹</button>
+      <Slider ref={sliderRef} {...settings}>
+        <div className="slide">
+          <img src={slide1} alt="" />
+        </div>
+        <div className="slide">
+          <img src={slide2} alt="" />
+        </div>
+        <div className="slide">
+          <img src={slide3} alt="" />
+        </div>
       </Slider>
     </div>
   );
