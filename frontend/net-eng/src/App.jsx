941dd9b6a28ed0c8868e6c3ce8b4123a0b9486e0
unknown <romina.khanmohammadi99@gmail.com>
2025-12-18 12:52:06 +0330

    feat: login added

--- frontend/net-eng/src/App.jsx
+++ frontend/net-eng/src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage';
-import SignupPage from './pages/SignupPage'; // فایل صفحه ثبت‌نام را وارد کنید
+import SignupPage from './pages/SignupPage'; 
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage/>}/>
       </Routes>
     </BrowserRouter>
   );
