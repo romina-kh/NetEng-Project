dca2c363dfaef67692f0c9ad298f5bed5cb7acde
arya237 <naderiarya20@gmail.com>
2025-12-18 01:39:00 -0800

    feat: signup completed

--- frontend/net-eng/src/pages/SignupPage.jsx
+++ frontend/net-eng/src/pages/SignupPage.jsx
@@ -5,11 +5,15 @@ function SignupPage(){
     return (
         <div className={styles.page}>
             <div className={styles.container}>
+                <h2>ثبت نام</h2>
                 <div className={styles.form}>
                     <input type="text" required placeholder="نام"/>
                     <input type="text" required placeholder="نام خانوادگی"/>
                     <input type="tel" pattern="[0][9][0-9]{9}" required placeholder="شماره تلفن"/>
                     <input type="password" required placeholder="رمز عبور"/>
+                    <button>ثبت</button>
+                    <p>ثبت نام کرده اید؟ <a href="/login">ورود</a> </p>
+                    
                 </div>
             </div>
         </div>
