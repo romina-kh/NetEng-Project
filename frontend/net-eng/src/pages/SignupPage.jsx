2e95f808dfc8b8943d7a7d6ca706586e94086a0d
arya237 <naderiarya20@gmail.com>
2025-12-20 13:20:12 -0800

    style: change singup style

--- frontend/net-eng/src/pages/SignupPage.jsx
+++ frontend/net-eng/src/pages/SignupPage.jsx
@@ -1,23 +1,30 @@
 import React from "react";
-import styles from '../styles/signupPage/Signup.module.css';
+import styles from "../styles/signupPage/Signup.module.css";
 
-function SignupPage(){
-    return (
-        <div className={styles.page}>
-            <div className={styles.container}>
-                <h2>ثبت نام</h2>
-                <div className={styles.form}>
-                    <input type="text" required placeholder="نام"/>
-                    <input type="text" required placeholder="نام خانوادگی"/>
-                    <input type="tel" pattern="[0][9][0-9]{9}" required placeholder="شماره تلفن"/>
-                    <input type="password" required placeholder="رمز عبور"/>
-                    <button>ثبت</button>
-                    <p>ثبت نام کرده اید؟ <a href="/login">ورود</a> </p>
-                </div>
-            </div>
-        </div>
-    );
+export default function Signup() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}></div>
+      <div className={styles.bottom}></div>
+
+      <div className={styles.center}>
+        <h2>ثبت‌نام در تک‌یار</h2>
+
+        <input type="text" placeholder="نام" />
+        <input type="text" placeholder="نام خانوادگی" />
+        <input
+          type="tel"
+          placeholder="شماره تلفن"
+          pattern="[0][9][0-9]{9}"
+        />
+        <input type="password" placeholder="رمز عبور" />
+
+        <button className={styles.signupBtn}>ثبت‌نام</button>
+
+        <p className={styles.link}>
+          قبلاً ثبت‌نام کرده‌اید؟ <a href="/login">ورود</a>
+        </p>
+      </div>
+    </div>
+  );
 }
-
-export default SignupPage
-
