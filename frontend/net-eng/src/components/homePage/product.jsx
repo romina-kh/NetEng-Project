417670f9e0aa8ecf437298809d9ff71330a56582
unknown <romina.khanmohammadi99@gmail.com>
2025-12-23 15:28:38 +0330

    style: product section

--- frontend/net-eng/src/components/homePage/product.jsx
+++ frontend/net-eng/src/components/homePage/product.jsx
@@ -12,22 +12,48 @@ export default function Products() {
       <div className="products-grid">
         <div className="product-card">
           <img src={product1} alt="product" />
-          <h3>هدفون بی‌سیم حرفه‌ای</h3>
-          <p>کیفیت صدای عالی، طراحی مدرن و مناسب استفاده روزمره</p>
+          <h3>هدست بلوتوثی انکر مدل Soundcore Q11i</h3>
+          <p>فاقد قابلیت نویز کنسلینگ</p>
+          <span>۳,۱۵۰,۰۰۰ تومان</span>
         </div>
 
         <div className="product-card">
           <img src={product2} alt="product" />
           <h3>ساعت هوشمند نسل جدید</h3>
           <p>پایش سلامتی، باتری قدرتمند و سازگار با اندروید و iOS</p>
+          <span>۳,۱۵۰,۰۰۰ تومان</span>
         </div>
 
         <div className="product-card">
           <img src={product3} alt="product" />
           <h3>اسپیکر بلوتوث قابل حمل</h3>
           <p>صدای شفاف، سبک و مناسب سفر و فضای باز</p>
+          <span>۳,۱۵۰,۰۰۰ تومان</span>
         </div>
+
+        <div className="product-card">
+          <img src={product2} alt="product" />
+          <h3>ساعت هوشمند نسل جدید</h3>
+          <p>پایش سلامتی، باتری قدرتمند و سازگار با اندروید و iOS</p>
+          <span>۳,۱۵۰,۰۰۰ تومان</span>
+        </div>
+
+        <div className="product-card">
+          <img src={product2} alt="product" />
+          <h3>ساعت هوشمند نسل جدید</h3>
+          <p>پایش سلامتی، باتری قدرتمند و سازگار با اندروید و iOS</p>
+          <span>۳,۱۵۰,۰۰۰ تومان</span>
+        </div>
+
+        <div className="product-card">
+          <img src={product2} alt="product" />
+          <h3>ساعت هوشمند نسل جدید</h3>
+          <p>پایش سلامتی، باتری قدرتمند و سازگار با اندروید و iOS</p>
+          <span>۳,۱۵۰,۰۰۰ تومان</span>
+        </div>
+
       </div>
+      
     </section>
   );
 }
