cfa2e01a360572050683f75302d8b5494c475bdc
unknown <romina.khanmohammadi99@gmail.com>
2026-02-10 22:27:28 +0330

    feat:login function added in service

--- backend/internal/repository/repo.go
+++ backend/internal/repository/repo.go
@@ -10,4 +10,5 @@ type UserRepository interface {
 	CreateUser(ctx context.Context, user model.User) error
 	ExistsByEmail(ctx context.Context, email string) (bool, error)
 	ExistsByPhonenumber(ctx context.Context, email string) (bool, error)
+	GetByEmailOrPhone(ctx context.Context, identifier string) (*model.User, error)
 }
--- backend/internal/service/user_service.go
+++ backend/internal/service/user_service.go
@@ -59,7 +59,20 @@ func (s *userservice) Signup(ctx context.Context, user model.User, password stri
 }
 
 
-func (s *userservice) login(ctx context.Context, username string, password string) (int, error) {
-	return 0, nil
+func (s *userservice) Login(ctx context.Context, identifier string, password string) (*model.User, error) {
+	user, err := s.repo.GetByEmailOrPhone(ctx, identifier)
+	if err != nil {
+		return nil, errors.New("invalid credentials")
+	}
 
+	err = bcrypt.CompareHashAndPassword(
+		[]byte(user.PasswordHash),
+		[]byte(password),
+	)
+	if err != nil {
+		return nil, errors.New("invalid credentials")
+	}
+
+	return user, nil
 }
+
