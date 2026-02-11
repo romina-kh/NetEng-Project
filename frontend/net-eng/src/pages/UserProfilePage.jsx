82fb98e4ca175d34e027cf88f7a392e32d7cb772
unknown <romina.khanmohammadi99@gmail.com>
2026-02-11 19:14:42 +0330

    feat + style :user dashboard added

--- frontend/net-eng/src/pages/UserProfilePage.jsx
+++ frontend/net-eng/src/pages/UserProfilePage.jsx
@@ -24,6 +24,7 @@ export default function Profile() {
     name: "عباس قادری",
     email: "abbas.ghaderi@gmail.com",
     phone: "09123456789",
+    address: "تهران - شریعتی - پلاک ۲",
     company: "تک‌یار",
     avatar: "https://i.pravatar.cc/150?img=3"
   });
@@ -32,96 +33,140 @@ export default function Profile() {
     setUser({ ...user, [e.target.name]: e.target.value });
   };
   return (
-    <div className={styles.container}>
-      <div className={styles.card}>
+  <div className={styles.container}>
 
-        {/* img*/}
-        <div className={styles.avatarSection}>
-          <img 
-            src={user.avatar}
-            alt="profile"
-            style={{ borderColor: themes[theme] }}
-          />
-          {isEditing && (
-            <button className={styles.changePhoto}>تغییر عکس</button>
-          )}
+    {/* shop */}
+    <div className={styles.shopContainer}>
+      <h3>داشبورد</h3>
+
+      <div className={styles.shopRow}>
+        <div className={styles.shopBox}>
+          <span>📦</span>
+          <div>
+            <h4>سفارش‌های من</h4>
+            <p>مشاهده و پیگیری سفارش‌ها</p>
+          </div>
         </div>
 
-        {/* info*/}
-        <div className={styles.infoSection}>
-        
-            {isEditing ? (
-                <>
-                    <input name="name" value={user.name} onChange={handleChange} />
-                    <input name="email" value={user.email} onChange={handleChange} />
-                    <input name="phone" value={user.phone} onChange={handleChange} />
-                    <input name="company" value={user.company} onChange={handleChange} />
-                </>
-            ) : (
-                <>
-                    <h2>{user.name}</h2>
-                    <p>{user.email} ✉︎</p>
-                    <p>{user.phone} 🕻</p>
-                    <p>{user.company}</p>
-                </>
-            )}
-        
+        <div className={styles.shopBox}>
+          <span>🛒</span>
+          <div>
+            <h4>سبد خرید</h4>
+            <p>کالاهای انتخاب شده</p>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.shopRow}>
+        <div className={styles.shopBox}>
+          <span>❤️</span>
+          <div>
+            <h4>علاقه‌مندی‌ها</h4>
+            <p>لیست محصولات ذخیره شده</p>
+          </div>
         </div>
 
-        {/* buttons */}
-        <div className={styles.actions}>
-          {isEditing ?(
-            <>
-                <button
-                    className={styles.saveBtn}
-                    style={{background: themes[theme]}}
-                    onClick={() => setIsEditing(false)}
-                >
-                    ذخیره تغییرات
-                </button>
-                <button
-                    className={styles.cancelBtn}
-                    style={{background: themes[theme]}}
-                    onClick={() => setIsEditing(false)}
-                >
-                    انصراف
-                </button>
-            </>
-            ) : (
-                    <button 
-                      className={styles.editBtn}
-                      style={{background: themes[theme]}}
-                      onClick={() => setIsEditing(true)}
-                    >
-                      ویرایش پروفایل
-                    </button>
-          )}  
+        <div className={styles.shopBox}>
+          <span>💳</span>
+          <div>
+            <h4>کیف پول</h4>
+            <p>مدیریت موجودی حساب</p>
+          </div>
         </div>
-
-        {/* setting */}
-        <div className={styles.settings}>
-            <h3>تنظیمات حساب</h3>
-            <button>تغییر رمز عبور</button>
-            <button>مدیریت اعلان‌ها</button>
-            <button className={styles.logout}>خروج از حساب</button>
-        </div>
-        
-
-        {/* theme */}
-        <div className={styles.themeSelector}>
-          <p>انتخاب تم:</p>
-
-          {Object.keys(themes).map((key) => (
-            <span
-              key={key}
-              className={styles.colorCircle}
-              style={{ background: themes[key] }}
-              onClick={() => setTheme(key)}
-            />
-          ))}
-        </div>
-
       </div>
     </div>
-  );
+
+    {/* profile card */}
+    <div className={styles.card}>
+
+      {/* img */}
+      <div className={styles.avatarSection}>
+        <img 
+          src={user.avatar}
+          alt="profile"
+          style={{ borderColor: themes[theme] }}
+        />
+        {isEditing && (
+          <button className={styles.changePhoto}>تغییر عکس</button>
+        )}
+      </div>
+
+      {/* info */}
+      <div className={styles.infoSection}>
+        {isEditing ? (
+          <>
+            <input name="name" value={user.name} onChange={handleChange} />
+            <input name="email" value={user.email} onChange={handleChange} />
+            <input name="phone" value={user.phone} onChange={handleChange} />
+            <input name="address" value={user.address} onChange={handleChange} />
+            <input name="company" value={user.company} onChange={handleChange} />
+          </>
+        ) : (
+          <>
+            <h2>{user.name}</h2>
+            <p>{user.email} ✉︎ </p>
+            <p>{user.phone} 🕻</p>
+            <p> 📍 {user.address}</p>
+            <p> 🌐 {user.company}</p>
+          </>
+        )}
+      </div>
+
+      {/* buttons */}
+      <div className={styles.actions}>
+        {isEditing ? (
+          <>
+            <button
+              className={styles.saveBtn}
+              style={{ background: themes[theme] }}
+              onClick={() => setIsEditing(false)}
+            >
+              ذخیره تغییرات
+            </button>
+            <button
+              className={styles.cancelBtn}
+              style={{ background: themes[theme] }}
+              onClick={() => setIsEditing(false)}
+            >
+              انصراف
+            </button>
+          </>
+        ) : (
+          <button 
+            className={styles.editBtn}
+            style={{ background: themes[theme] }}
+            onClick={() => setIsEditing(true)}
+          >
+            ویرایش پروفایل
+          </button>
+        )}
+      </div>
+
+      {/* setting */}
+      <div className={styles.settings}>
+        <h3>تنظیمات حساب</h3>
+        <button>تغییر رمز عبور</button>
+        <button>مدیریت اعلان‌ها</button>
+        <button className={styles.logout}>خروج از حساب</button>
+      </div>
+
+      {/* theme */}
+      <div className={styles.themeSelector}>
+        <p>انتخاب تم:</p>
+        {Object.keys(themes).map((key) => (
+          <span
+            key={key}
+            className={styles.colorCircle}
+            style={{ background: themes[key] }}
+            onClick={() => setTheme(key)}
+          />
+        ))}
+      </div>
+
+    </div>
+
+    
+  </div>
+);
+
 }
