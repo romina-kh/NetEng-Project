576f5dd53bd71a2bab7c8bb73ada2991e441cdd1
arya237 <naderiarya20@gmail.com>
2026-02-19 20:29:06 +0330

    feat: implemented product page

--- frontend/net-eng/src/App.jsx
+++ frontend/net-eng/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import HomePage from './pages/HomePage';
 import SignupPage from './pages/SignupPage'; 
 import LoginPage from './pages/LoginPage';
+import ProductPage from './pages/ProductPage'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/login" element={<LoginPage/>}/>
+        <Route path= "/product" element={<ProductPage/>} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/net-eng/src/pages/ProductPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Product from "../components/productPage/productSection";
+
+
+
+export default function ProductPage() {
+  return (
+    <>
+       <Product />
+    </>
+  );
+}
